3f9b57440d1f8b7b9e66b7d643738f91bc52222b
Guilherme Lopes <guisilva.070104@gmail.com>
2024-07-28 01:20:45 -0300

    feat: edit progressbar music

--- app/page.tsx
+++ app/page.tsx
@@ -261,7 +261,7 @@ export default function Home() {
           <div className="flex items-center gap-2">
             <span className="text-xs text-zinc-400">0:31</span>
             <div className="h-1 rounded-full w-96 bg-zinc-600">
-              <div className="h-1 rounded-full w-40 bg-zinc-200"></div>
+              <div className="h-1 rounded-full w-10 bg-zinc-200"></div>
             </div>
             <span className="text-xs text-zinc-400">2:19</span>
           </div>
