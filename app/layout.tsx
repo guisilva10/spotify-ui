94b5f486cdc0ccd9e37056588bcf32566649f9a3
Guilherme Lopes <guisilva.070104@gmail.com>
2024-07-29 01:54:13 -0300

    feat: format default prettier on

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
-
 export const metadata: Metadata = {
   title: "Spotify",
   description: "Listen to music",
--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-       <Aside/>
-      <Main/>
+        <Aside />
+        <Main />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
